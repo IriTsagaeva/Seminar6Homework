f48cb4cf303bb3785c388256b1359ed15a171938
IrinaTsagaeva <irina-hadaeva@yandex.ru>
2022-11-21 12:22:51 +0300

    Task1 added

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿int ReturnPositiveElementsNumber()
+{
+    Console.WriteLine("Пожалуйста, введите число:");
+    bool text = Int32.TryParse(Console.ReadLine(), out int intNumber);
+    if (!text)
+    {
+        return 0;
+    }
+    else if (intNumber > 0)
+    {
+        return 1 + ReturnPositiveElementsNumber();
+    }
+    else
+    {
+        return ReturnPositiveElementsNumber();
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Пожалуйста, введите числа. Для окончания ввода введите любой отличный от числа символ:");
+Console.WriteLine($"Количество введенных положительных элементов равно {ReturnPositiveElementsNumber()}.");
